1f726cc74b223a3565eed584dc84abb4cd7d4fd2
Sean Toner <placeoftheway@gmail.com>
2022-05-08 15:00:20 -0400

    Added exploding as function to DieTrait

--- retro/src/tasks/probability.rs
+++ retro/src/tasks/probability.rs
@@ -23,7 +23,44 @@ use rand::Rng;
 pub trait DieTraits {
     // fn value(self: Self, calc: Box<dyn Fn(u32) -> u32>) -> Self;
     // fn exploding(self: Self, opt: Option<u32>) -> Self;
+
+    /// Generates a roll of the die
     fn roll(self: &Self, num: u32) -> Vec<u32>;
+
+    /// Calculates how many successes there are
+    fn get_successes(self: &Self, dice: u32, accum: impl FnMut(u32, &u32) -> u32) -> (u32, Vec<u32>) {
+        let roll = self.roll(dice);
+        let successes = roll.iter().fold(0, accum);
+        (successes, roll)
+    }
+
+    fn exploding(self: &Self, roll: &mut Vec<u32>, thresh: u32, die: impl Fn(u32) -> Vec<u32>) -> Vec<u32> {
+        //&mut let mut roll = self.roll(num);
+        let mut eroll: Vec<u32> = vec![];
+    
+        // Calculate which rolls are greater than threshold.  These dice will get extra rolls
+        for d in roll.iter() {
+            if *d >= thresh {
+                eroll.append(&mut die(1));
+            }
+        }
+    
+        let mut exploded = if !eroll.is_empty() {
+            let mut new_roll = exploding(&mut eroll, thresh, die);
+            eroll.append(&mut new_roll);
+            eroll
+        } else {
+            eroll
+        };
+    
+        // This is kind of expensive to do, but I think this is better than returning a reference
+        roll.append(&mut exploded);
+        let mut final_roll: Vec<u32> = vec![];
+        for r in roll {
+            final_roll.push(*r);
+        }
+        final_roll
+    }
 }
 
 pub struct DiePool {
@@ -104,6 +141,7 @@ pub fn id<T>(x: T) -> T {
     x
 }
 
+/// Returns a new Vec of u32, where for each value in the original roll, if it is >= thresh, a new die roll is added
 pub fn explode(roll: &Vec<u32>, thresh: u32, die: impl Fn(u32) -> Vec<u32>) -> Vec<u32> {
     let mut eroll: Vec<u32> = vec![];
     for d in roll {
@@ -115,7 +153,7 @@ pub fn explode(roll: &Vec<u32>, thresh: u32, die: impl Fn(u32) -> Vec<u32>) -> V
     eroll
 }
 
-/// Searches through the dice roll and checks if any values <= to the threshold
+/// Searches through the dice roll and checks if any values >= to the threshold
 ///
 /// If any are, it will create roll one die, for each value >= to the thresh.  It will recursively check this new list
 /// of die roll(s).  Once there are no more exploding dice, it will append the rolls all together.
@@ -123,7 +161,7 @@ pub fn exploding(roll: &mut Vec<u32>, thresh: u32, die: impl Fn(u32) -> Vec<u32>
     let mut eroll: Vec<u32> = vec![];
 
     for d in roll.iter() {
-        if *d <= thresh {
+        if *d >= thresh {
             eroll.append(&mut die(1));
         }
     }
@@ -160,12 +198,12 @@ mod tests {
         println!("roll {:?}", roll);
     }
 
-    fn get_successes(pool: &DiePool, dice: u32, target: u32) -> (u32, Vec<u32>) {
+    fn _get_successes(pool: &DiePool, dice: u32, target: u32) -> (u32, Vec<u32>) {
         let roll = pool.roll(dice);
         let successes = roll.iter().fold(
             0u32,
             |acc, next| {
-                if *next <= target {
+                if *next >= target {
                     acc + 1
                 } else {
                     acc
@@ -178,15 +216,22 @@ mod tests {
     #[test]
     fn average10of20() {
         let mut avg: Vec<u32> = vec![];
-        let pool = DiePool::pool(20).exploding(Some(20));
-        for n in 0..10 {
-            let (successes, roll) = get_successes(&pool, 6, 10);
+        let pool = DiePool::pool(20).exploding(Some(19));
+        for n in 0..100 {
+            let (successes, roll) = pool.get_successes(6, |acc, next| {
+                if *next >= 11 {
+                    acc + 1
+                } else {
+                    acc
+                }
+            });
+            //let (successes, roll) = get_successes(&pool, 6, 10);
             avg.push(successes);
             println!("{}: Successes = {} from {:?}", n, successes, roll);
         }
 
         let sum_avg = avg.iter().fold(0u32, |acc, next| acc + next) as f32;
-        let calc_avg = sum_avg / 100000.0;
+        let calc_avg = sum_avg / 100.0;
         println!("Sum = {}, Calculated average is {}", sum_avg, calc_avg);
 
         let mut scores: HashMap<u32, u32> = HashMap::new();
