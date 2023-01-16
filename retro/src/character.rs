decad1a4b7886034ede57e27f04277a8f007e444
Sean Toner <placeoftheway@gmail.com>
2023-01-16 01:46:36 -0500

    Updated deps

--- retro/src/character.rs
+++ retro/src/character.rs
@@ -1,1 +1,9 @@
 //! Represents a Character in retro
+//! 
+//! # Making a Player Character
+//! 
+//! To create a character for retro, it's a good idea to first understand the components that make up a character.
+//! 
+//! - Attributes
+//! - Skills
+//! - 
